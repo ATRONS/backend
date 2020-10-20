b33aa2830ed6dd89f6c0538ed6855ca188a4f1ee
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-10-20 18:31:37 +0300

    refining model

--- models/user.js
+++ models/user.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('../helpers/validator');
 const _ = require('lodash');
 
 const COLLECTION = 'users';
+const SALT_FACTOR = 10;
 
 const UserSchema = mongoose.Schema({
     firstname: { type: String, required: true, trim: true },
     lastname: { type: String, required: true, trim: true },
     gender: { type: String, required: true, enum: ['MALE', 'FEMALE'] },
-    email: { type: String, required: true, trim: true, lowercase: true },
+    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     avatar_url: { type: String },
 
     role: { type: mongoose.Schema.Types.ObjectId, ref: 'roles', required: true },
-    password: { type: String, required: true, select: false },
+    password: { type: String, required: true, min: 8, select: false },
     tokens: { type: [String], select: false },
 
     verify_email_hash: { type: String, select: false },
@@ -22,7 +24,7 @@ const UserSchema = mongoose.Schema({
     otp_key: { type: String, select: false },
     otp_expire: { type: Number, select: false },
 
-    email_verified: { type: Boolean, default: false },
+    verified: { type: Boolean, default: false },
     active: { type: Boolean, default: true },
     deleted: { type: Boolean, default: false },
 }, { timeStamp: { createdAt: 'created_at', updatedAt: 'updated_at' } });
@@ -35,6 +37,32 @@ const autoPopulateRole = function (next) {
 }
 
 UserSchema.pre('findOne', autoPopulateRole).pre('find', autoPopulateRole);
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            return next();
+        });
+    });
+});
+
+// --------------------------------------------------------------------------
+UserSchema.methods.isPasswordCorrect = function (password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+UserSchema.methods.addSessionId = function (sessionId) {
+    this.tokens.push(sessionId);
+}
 
 // --------------------------------------------------------------------------
 UserSchema.statics.getUserByVerifyEmailHash = function (hash, callback) {
