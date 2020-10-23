68209cb79b473bd7b052e284efc570c154c13a30
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-10-23 18:45:09 +0300

    fleshing out end points

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const providerCtrl = require('../controllers/provider');
 const readerCtrl = require('../controllers/reader');
 const genericCtrl = require('../controllers/generic');
 
+const authMiddleware = require('../middleware/auth');
+
 const router = require('express').Router();
 
 // ------------------------ common area -----------------------------
@@ -21,20 +23,71 @@ router.post(
 
 router.post(
     readerBase + '/logout',
+    authMiddleware.authenticateReader,
     readerCtrl.logout);
 
 router.post(
     readerBase + '/forgotPassword',
     readerCtrl.forgotPassword);
 
+router.get(
+    readerBase + '/initialData',
+    authMiddleware.authenticateReader,
+    readerCtrl.initialData);
+
 router.put(
     readerBase + '/profile',
+    authMiddleware.authenticateReader,
     readerCtrl.updateProfile);
 
 // ------------------------- provider area --------------------------
 const providerBase = '/provider';
+router.post(
+    providerBase + '/login',
+    providerCtrl.login);
+
+router.post(
+    providerBase + '/logout',
+    authMiddleware.authenticateProvider,
+    providerCtrl.logout);
+
+router.post(
+    providerBase + '/forgotPassword',
+    providerCtrl.forgotPassword);
+
+router.get(
+    providerBase + '/initialData',
+    authMiddleware.authenticateProvider,
+    providerCtrl.initialData);
+
+router.put(
+    providerBase + '/profile',
+    authMiddleware.authenticateProvider,
+    providerCtrl.updateProfile);
 
 // ------------------------- admin area -----------------------------
 const adminBase = '/admin';
+router.post(
+    adminBase + '/login',
+    adminCtrl.login);
+
+router.post(
+    adminBase + '/logout',
+    authMiddleware.authenticateAdmin,
+    adminCtrl.logout);
+
+router.post(
+    adminBase + '/forgotPassword',
+    adminCtrl.forgotPassword);
+
+router.get(
+    adminBase + '/initialData',
+    authMiddleware.authenticateAdmin,
+    adminCtrl.initialData);
+
+router.put(
+    adminBase + '/profile',
+    authMiddleware.authenticateAdmin,
+    adminCtrl.updateProfile);
 
 module.exports = router;
