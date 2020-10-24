62dccdaac7de42363613f6c8aaaafcabd6ea2636
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-10-24 15:23:10 +0300

    added createDefaultAdmin

--- database/db.js
+++ database/db.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const AdminSchema = require('../models/users/admin');
+
+const logger = global.logger;
 
 const dbCtrl = {};
 
@@ -10,9 +13,41 @@ dbCtrl.init = function (url, callback) {
         useFindAndModify: false,
 
     }, (err) => {
-        if (err) return callback(err);
+        if (err) {
+            logger.error(err);
+            return callback(err);
+        }
+
+        logger.info('connected to db');
         return callback();
     });
+}
+
+dbCtrl.createDefaultAdmin = function (callback) {
+    const defaultAdmin = new AdminSchema({
+        firstname: 'tewodros',
+        lastname: 'dagnen',
+        email: 'admin@atrons.com',
+        avatar_url: '/media/default_admin.png',
+        auth: {
+            password: 'password'
+        }
+    });
+
+    defaultAdmin.save((err, result) => {
+        if (err) {
+            if (err.code) {
+                logger.info('admin already created');
+                return callback(null, null);
+            }
+
+            logger.error(err);
+            return callback(err, null);
+        }
+
+        logger.info('default admin created');
+        return callback(null, null);
+    })
 }
 
 module.exports = dbCtrl;
