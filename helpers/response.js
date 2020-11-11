14219f2aae1b4dcbc334ddc5c5a1e4512b85b308
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-11-11 11:19:14 +0300

    added a generic errorResponse ahndler

--- helpers/response.js
+++ helpers/response.js
@@ -1,4 +1,5 @@
 const response = {};
+const logger = global.logger;
 
 response.success = function (res, data, status = 200) {
     res.status(status).json({ success: true, data });
@@ -6,6 +7,14 @@ response.success = function (res, data, status = 200) {
 
 response.failure = function (res, message, status = 400) {
     res.status(status).json({ success: false, message });
+}
+
+response.errorResponse = function (err, res) {
+    if (err.code) return failure(res, 'Email Already taken');
+    if (err.errors) return response.failure(res, err);
+    if (err.custom) return response.failure(res, err.custom, err.status);
+    logger.error(err);
+    return response.failure(res, 'Internal Error', 500);
 }
 
 module.exports = response;
