3c3587cdfb1a322aeab408d596e04e3a2f546236
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-11-12 10:43:15 +0300

    outsourced createDefaultAdmin

--- database/db.js
+++ database/db.js
@@ -23,31 +23,4 @@ dbCtrl.init = function (url, callback) {
     });
 }
 
-dbCtrl.createDefaultAdmin = function (callback) {
-    const defaultAdmin = new AdminSchema({
-        firstname: 'tewodros',
-        lastname: 'dagnen',
-        email: 'admin@atrons.com',
-        avatar_url: '/media/default_admin.png',
-        auth: {
-            password: 'password'
-        }
-    });
-
-    defaultAdmin.save((err, result) => {
-        if (err) {
-            if (err.code) {
-                logger.info('admin already created');
-                return callback(null, null);
-            }
-
-            logger.error(err);
-            return callback(err, null);
-        }
-
-        logger.info('default admin created');
-        return callback(null, null);
-    })
-}
-
 module.exports = dbCtrl;
