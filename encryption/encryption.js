054543a413d37305d8c5154e9fb73579a7e76282
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-11-22 18:37:55 +0300

    initial commit

---
+++ encryption/encryption.js
@@ -0,0 +1,22 @@
+const crypto = require('crypto');
+const { pipeline } = require('stream');
+
+const algorithm = 'aes-256-cbc';
+const IV_LENGTH = 16;
+const KEY_LENGTH = 32;
+
+ctrl = {};
+
+ctrl.generateEncrKeyAndIV = function () {
+    return {
+        key: crypto.randomBytes(KEY_LENGTH),
+        iv: crypto.randomBytes(IV_LENGTH),
+    }
+}
+
+ctrl.encryptAndPipe = function (input, output, keyIV, errCallback) {
+    const cipher = crypto.createCipheriv(algorithm, keyIV.key, keyIV.iv);
+    return pipeline(input, cipher, output, errCallback);
+}
+
+module.exports = ctrl;
