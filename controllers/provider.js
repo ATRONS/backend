a24752788e9a7da8b860624cffd8868593bca481
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-11-23 16:08:48 +0300

    added getOwnMaterials and getEarningsByMaterials

--- controllers/provider.js
+++ controllers/provider.js
@@ -1,5 +1,8 @@
 const ProviderSchema = require('../models/users/provider');
+const MaterialSchema = require('../models/material');
+const TransactionSchema = require('../models/transaction');
 const genericCtrl = require('./generic');
+const { errorResponse, success, defaultHandler } = require('../helpers/response');
 
 const ctrl = {};
 
@@ -23,6 +26,41 @@ ctrl.initialData = function (req, res, next) {
     };
 
     success(res, response);
+}
+
+ctrl.getOwnMaterials = function (req, res, next) {
+    req.query.provider = "5fb7ec05faf68e1b00fe7f3c"; //Bealu Girma's Id.
+    MaterialSchema.minifiedSearch(req.query, defaultHandler(res));
+}
+
+ctrl.getEarningsByMaterials = function (req, res, next) {
+    req.query.provider = "5fb7ec05faf68e1b00fe7f3c"; //Bealu Girma's Id.
+
+    MaterialSchema.minifiedSearch(req.query, (err, matResult) => {
+        if (err) return errorResponse(err, res);
+        if (!matResult.materials.length) return success(res, []);
+
+        const matIds = matResult.materials.map((each) => each._id);
+        TransactionSchema.earningsByMaterials(matIds, (err, earnings) => {
+            if (err) return errorResponse(err, res);
+
+            const earningsObj = {};
+            earnings.forEach((earning) => earningsObj[earning._id] = earning);
+            matResult.materials = matResult.materials.map((material) => {
+                let earning = earningsObj[material._id];
+                return {
+                    _id: material._id,
+                    title: material.title,
+                    subtitle: material.subtitle,
+                    amount: earning ? earning.amount : 0,
+                    count: earning ? earning.count : 0,
+                };
+            });
+            return success(res, matResult);
+        });
+
+    });
+
 }
 
 ctrl.uploadFile = genericCtrl.uploadFile;
