4b2ae97d3c024debdd5113f520497bf382eb906f
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-12-01 16:55:44 +0300

    added authorization middlewares

--- routes/index.js
+++ routes/index.js
@@ -47,6 +47,7 @@ router.post(
 router.post(
     readerBase + '/logout',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.logout);
 
 router.post(
@@ -56,47 +57,56 @@ router.post(
 router.get(
     readerBase + '/initialData',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.initialData);
 
 router.put(
     readerBase + '/profile',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.updateProfile);
 
 router.post(
     readerBase + '/upload/image',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     imgUpload.single('image'),
     readerCtrl.uploadFile);
 
 router.get(
     readerBase + '/materials',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.searchMaterials);
 
 router.get(
     readerBase + '/materials/tags',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.getAllTags);
 
 router.get(
     readerBase + '/materials/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.getMaterial);
 
 router.post(
     readerBase + '/materials/:id/purchase',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.purchaseMaterial);
 
 router.get(
     readerBase + '/providers',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.searchProviders);
 
 router.get(
     readerBase + '/providers/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isReader,
     readerCtrl.getProvider);
 
 // ------------------------- provider area --------------------------
@@ -109,27 +119,38 @@ router.post(
 router.get(
     providerBase + '/initialData',
     authMiddleware.authenticateUser,
+    authMiddleware.isProvider,
     providerCtrl.initialData);
 
 router.put(
     providerBase + '/profile',
     authMiddleware.authenticateUser,
+    authMiddleware.isProvider,
     providerCtrl.updateProfile);
 
 router.post(
     providerBase + '/upload/image',
     authMiddleware.authenticateUser,
+    authMiddleware.isProvider,
     imgUpload.single('image'),
     providerCtrl.uploadFile);
 
 router.get(
     providerBase + '/materials',
     authMiddleware.authenticateUser,
+    authMiddleware.isProvider,
     providerCtrl.getOwnMaterials);
+
+router.get(
+    providerBase + '/materials/:id',
+    authMiddleware.authenticateUser,
+    authMiddleware.isProvider,
+    providerCtrl.getMaterial);
 
 router.get(
     providerBase + '/earnings',
     authMiddleware.authenticateUser,
+    authMiddleware.isProvider,
     providerCtrl.getEarningsByMaterials);
 
 // ------------------------- admin area -----------------------------
@@ -142,77 +163,92 @@ router.post(
 router.put(
     adminBase + '/profile',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.updateProfile);
 
 router.get(
     adminBase + '/initialData',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.initialData);
 
 router.get(
     adminBase + '/users/providers',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.searchProviders);
 
 router.get(
     adminBase + '/users/providers/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.getProvider);
 
 router.post(
     adminBase + '/users/providers',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.createProvider);
 
 router.put(
     adminBase + '/users/providers/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.updateProviderInfo);
 
 router.delete(
     adminBase + '/users/providers/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.deleteProvider);
 
 router.post(
     adminBase + '/materials',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.createMaterial);
 
 router.get(
     adminBase + '/materials',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.searchMaterials);
 
 router.get(
     adminBase + '/materials/tags',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.getAllTags);
 
 router.get(
     adminBase + '/materials/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.getMaterial);
 
 router.put(
     adminBase + '/materials/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.updateMaterial);
 
 router.delete(
     adminBase + '/materials/:id',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     adminCtrl.deleteMaterial);
 
 router.post(
     adminBase + '/upload/material',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     materialUpload.single('material'),
     adminCtrl.uploadFile);
 
 router.post(
     adminBase + '/upload/image',
     authMiddleware.authenticateUser,
+    authMiddleware.isAdmin,
     imgUpload.single('image'),
     adminCtrl.uploadFile);
 
