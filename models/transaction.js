52841d7fda502a5f678f2d5e12f4be7d2b78a1a9
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-12-01 23:42:40 +0300

    added earningsByProviderBnDays

--- models/transaction.js
+++ models/transaction.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
 const luxon = require('luxon');
 
 const COLLECTION = 'transactions';
 const LIMIT = 10;
 
 const TransactionSchema = mongoose.Schema({
-    reader: { type: mongoose.Types.ObjectId, required: true, index: true },
-    provider: { type: mongoose.Types.ObjectId, required: true, index: true },
-    material: { type: mongoose.Types.ObjectId, required: true, index: true },
+    reader: { type: mongoose.Types.ObjectId, required: true, ref: 'readers', index: true },
+    provider: { type: mongoose.Types.ObjectId, required: true, ref: 'providers', index: true },
+    material: { type: mongoose.Types.ObjectId, required: true, ref: 'materials', index: true },
 
     amount: { type: Number, required: true, min: 0 },
     transaction_fee: { type: Number, default: 0 },
@@ -158,6 +159,85 @@ TransactionSchema.statics.earningByMaterial = function (matId, callback) {
             }
         }
     ]).exec(callback);
+}
+
+TransactionSchema.statics.earningsByProviderBnDays = function (provider, filters, callback) {
+    if (!_.isString(filters.startDate) || !_.isString(filters.endDate)) {
+        return callback({ custom: 'startDate and endDate query params required', status: 400 });
+    }
+
+    const start = luxon.DateTime.fromISO(filters.startDate);
+    const end = luxon.DateTime.fromISO(filters.endDate);
+    console.log(start.valueOf());
+    console.log(end.valueOf());
+
+    if (_.isNaN(start.valueOf()) || _.isNaN(end.valueOf())) {
+        return callback({ custom: 'Invalid startDate or endDate', status: 400 });
+    }
+
+    if (start.valueOf() > end.valueOf()) {
+        return callback({ custom: 'startDate cannot be greater than endDate', status: 400 });
+    }
+
+    const query = {
+        provider: provider,
+        created_at: {
+            $gte: start,
+            $lte: end,
+        }
+    }
+    this.model(COLLECTION)
+        .find(query)
+        .sort({ createdAt: 1 })
+        .populate('material', { title: 1, subtitle: 1, _id: 1 })
+        .lean()
+        .exec((err, transactions) => {
+            if (err) return callback(err);
+            const perDay = {};
+
+            transactions.map((each) => {
+                let date = luxon.DateTime.fromJSDate(each.created_at);
+                each.dateKey = `${date.year}-${date.month}-${date.day}`;
+                return each;
+            }).forEach(each => {
+                let key = each.dateKey;
+                let _id = each.material._id.toHexString();
+
+                if (!perDay[key]) perDay[key] = {};
+                if (!perDay[key][_id]) perDay[key][_id] = [];
+
+                perDay[key][_id].push(each);
+            });
+
+            const response = [];
+            for (let date of Object.keys(perDay)) {
+                let aDaysTransactions = [];
+                for (let matId of Object.keys(perDay[date])) {
+                    let material = {
+                        title: '',
+                        subtitle: '',
+                        _id: '',
+                        earning: 0,
+                        number_of_items_sold: 0,
+                    };
+                    let transactions = perDay[date][matId];
+                    for (trans of transactions) {
+                        material.earning += trans.amount;
+                        material.title = trans.material.title;
+                        material.subtitle = trans.material.subtitle;
+                        material._id = trans.material._id;
+                        material.number_of_items_sold++;
+                    }
+                    aDaysTransactions.push(material);
+                }
+                response.push({
+                    day_label: date,
+                    earnings: aDaysTransactions,
+                });
+            }
+
+            return callback(null, response);
+        });
 }
 
 module.exports = mongoose.model(COLLECTION, TransactionSchema);
