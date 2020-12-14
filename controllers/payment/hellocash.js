9ee7eb66b2bf35fb65fe47f1f03b68be5364d816
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-12-14 15:58:09 +0300

    laid out work flow, not tested

--- controllers/payment/hellocash.js
+++ controllers/payment/hellocash.js
@@ -1,10 +1,13 @@
 const urls = require('../../helpers/constants/url');
 const InvoiceSchema = require('../../models/invoice');
 const TransactionSchema = require('../../models/transaction');
+const ProviderSchema = require('../../models/users/provider');
+const AtronsSchema = require('../../models/atrons');
 
 const crypto = require('crypto');
 const axios = require('axios');
 const asyncLib = require('async');
+const settings = require('../../defaults/settings');
 
 const ctrl = {};
 
@@ -62,75 +65,77 @@ ctrl.webHook = function (req, res, next) {
         transaction_dump: payload,
     };
 
-    // save PURCHASE or WITHDRAWAL transaction
-    // save 
-    // things to do
-    // - save a PURCHASE or WITHDRAWAL transaction
-    // - save a SERVICE_FEE transaction for the PURCHASE or WITHDRAWAL
-    // - update providers balance, also the companies balance.
-
-    asyncLib.waterfall([
-        function (callback) {
-            InvoiceSchema.updateByTracenumber(tracenumber, invoiceUpdates, callback);
-        },
-        // function
-        function (updatedInvoice, callback) {
-            const transactionInfo = {
-                kind: updatedInvoice.kind,
-                reader: updatedInvoice.reader,
-                provider: updatedInvoice.provider,
-                material: updatedInvoice.material,
-                amount: updatedInvoice.amount,
-                tracenumber: updatedInvoice.tracenumber,
-                transaction_fee: updatedInvoice.transaction_fee,
-            }
-            TransactionSchema.createTransaction(transactionInfo, callback);
-        },
-        function (transaction, callback) {
-            return callback(null, transaction);
-        }
-    ], function (err, finalResult) {
+    InvoiceSchema.updateByTracenumber(tracenumber, invoiceUpdates, (err, updatedInvoice) => {
         if (err) {
             logger.error(err);
-            logger.error('Transaction processing failed, tracenumber: ' + tracenumber);
+            logger.error('Invoice update failed, tracenumber: ' + tracenumber);
             return;
         }
 
-        logger.info('Transaction processing successfull tracenumber: ' + tracenumber);
-        logger.info(finalResult);
-    });
+        const tasks = [];
 
-    InvoiceSchema.findByTraceNumber(tracenumber, (err, invoice) => {
-        if (err) {
-            logger.error('Invoice fetch failed for tracenumber ' + tracenumber);
-            return;
-        }
-        if (!invoice) {
-            logger.error('Invoice does not exist, tracenumber ' + tracenumber);
-            return;
+        // this adds a PURCHASE or WITHDRAWAL transaction
+        if (updatedInvoice.kind === settings.INVOICE_TYPES.PURCHASE) {
+            const taxShare = updatedInvoice.amount * settings.TAX_FEE_PERCENT;
+            const atronsShare = updatedInvoice.amount * settings.ATRONS_SERVICE_FEE_PERCENT;
+            const providerShare = updatedInvoice.amount - (atronsShare + taxShare);
+
+            // update providers balance
+            tasks.push(function (callback) {
+                ProviderSchema.addBalance(updatedInvoice.provider, providerShare, callback);
+            });
+
+            // update companies balance and tax amount
+            tasks.push(function (callback) {
+                AtronsSchema.addBalanceAndTaxAmount(atronsShare, taxShare, callback);
+            });
+
+            // add PURCHASE transaction
+            tasks.push(function (callback) {
+                const transactionInfo = {
+                    kind: updatedInvoice.kind,
+                    reader: updatedInvoice.reader,
+                    provider: updatedInvoice.provider,
+                    material: updatedInvoice.material,
+                    amount: updatedInvoice.amount,
+                    tracenumber: updatedInvoice.tracenumber,
+                    transaction_fee: updatedInvoice.transaction_fee,
+                    description: 'Purchase for ' + updatedInvoice.material.title + ' ' + updatedInvoice.subtitle,
+                };
+                TransactionSchema.createTransaction(transactionInfo, callback);
+            });
+
+            // record SERVICE_FEE transaction
+            tasks.push(function (callback) {
+                const transactionInfo = {
+                    kind: settings.INVOICE_TYPES.SERVICE_FEE,
+                    amount: atronsShare,
+                    tracenumber: updatedInvoice.tracenumber,
+                    description: 'Service fee for ' + updatedInvoice.transaction_id,
+                };
+                TransactionSchema.createTransaction(transactionInfo, callback);
+            });
+
+            // record TAX_FEE transaction
+            tasks.push(function (callback) {
+                const transactionInfo = {
+                    kind: settings.INVOICE_TYPES.TAX_FEE,
+                    amount: taxShare,
+                    tracenumber: updatedInvoice.tracenumber,
+                    description: 'Tax fee for ' + updatedInvoice.transaction_id,
+                };
+                TransactionSchema.createTransaction(transactionInfo, callback);
+            });
         }
 
-        invoice.transaction_fee = payload.fee;
-        invoice.transaction_id = payload.id;
-        invoice.status = payload.status;
-        invoice.transaction_dump = payload;
-        invoice.save((err, savedInvoice) => {
+        asyncLib.parallel(tasks, (err, results) => {
             if (err) {
-                logger.error('Invoice save failed, tracenumber ' + tracenumber);
+                logger.error(err);
+                logger.error('Transaction processing failed, tracenumber: ' + tracenumber);
                 return;
             }
 
-            logger.debug('invoice save successfull, tracenumber ' + tracenumber);
-
-            const transaction = TransactionSchema();
-
-            transaction.save((err, savedTransaction) => {
-                if (err) {
-                    logger.error('transaction save failed for tracenumber ' + tracenumber);
-                    return;
-                }
-                logger.debug('transaction save successfull, tracenumber ' + tracenumber);
-            });
+            logger.debug('transaction processing completed, tracenumber: ' + tracenumber);
         });
     });
 }
